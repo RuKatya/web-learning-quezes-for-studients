6281fbdea8104c734e5ebd2fe1d5e6065543846e
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2023-04-06 13:48:23 +0300

    Update connectionToENV.js

--- utils/connectionToENV.js
+++ utils/connectionToENV.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 exports.connectionToENV = () => {
     if(process.env.NODE_ENV == "production") {
         require('dotenv').config({ path: path.join(__dirname, `../.env.${process.env.NODE_ENV}`)}); 
